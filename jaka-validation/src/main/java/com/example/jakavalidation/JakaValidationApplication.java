60542a64f3a8a4d18ecb9718732bc6229b14351c
Stack <stackqin@apifox.com>
2024-01-08 12:22:35 +0800

    测试

---
+++ jaka-validation/src/main/java/com/example/jakavalidation/JakaValidationApplication.java
@@ -0,0 +1,13 @@
+package com.example.jakavalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JakaValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JakaValidationApplication.class, args);
+    }
+
+}
